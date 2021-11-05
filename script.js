8544ef687a322aaa46df81a94508669d0d24d8b0
Jilbert Ebrahimi <jilbert@greymen.co>
2021-11-05 12:15:57 +0100

    Complete redesign of the code

--- script.js
+++ script.js
@@ -1,141 +1,39 @@
-// const navToggle = document.querySelectorAll(".mobile-menu-toggle");
-// const mobileMenu = document.querySelectorAll(".mobile-menu");
+//Menu
+const desktopNav = document.getElementsByClassName('desktop-nav');
+const mobileNav = document.getElementsByClassName('mobile-nav');
+const mobileMenu = document.getElementsByClassName('mobile-menu');
+const mobileMenuClose = document.getElementById('mobile-menu-close');
 
-// navToggle.addEventListener('click', function (event) {
-//   mobileMenu.classList.toggle('mobile-menu-active');
-// });
+function menuResize() {
+  
+  if (window.innerWidth <= 768) {
+    desktopNav[0].classList.add('hidden');
+    desktopNav[0].classList.remove('show');
 
-const imgDatabase = [
-    "1.jpg",
-    "2.jpg",
-    "3.jpg",
-    "4.jpg",
-    "5.jpg",
-    "6.jpg",
-    "7.jpg",
-    "8.jpg",
-    "9.jpg",
-    "10.jpg",
-    "11.jpg",
-    "12.jpg",
-    "13.jpg",
-    "14.jpg",
-    "15.jpg",
-    "16.jpg",
-    "17.jpg",
-    "18.jpg"
-]
-
-const prev = document.getElementById('button_left');
-const next = document.getElementById('button_right');
-const images = document.querySelector("img");
-
-const totalImages = imgDatabase.length;
-let index = 0;
-
-prev.addEventListener('click', () => {
-  nextImage('prev');
-})
-
-next.addEventListener('click', () => {
-  nextImage('next');
-})
-
-function nextImage(direction) {
-  if(direction == 'next') {
-    index++;
-    if(index == totalImages) {
-      index = 0;
-    }
+    mobileNav[0].classList.add('show');
+    mobileNav[0].classList.remove('hidden');
   } else {
-    if(index == 0) {
-      index = totalImages - 1;
-    } else {
-      index--;
-    }
-  }
+    desktopNav[0].classList.add('show');
+    desktopNav[0].classList.remove('hidden');
 
-  images.src = `assets/img/${imgDatabase[index]}`;
+    mobileNav[0].classList.add('hidden');
+    mobileNav[0].classList.remove('show');
+  }
 };
 
+window.onload = menuResize;
+window.addEventListener('resize', menuResize);
 
-// ---------------------------------------
+mobileNav[0].addEventListener('click', function(){
+  mobileMenu[0].classList.add('show');
+  mobileNav[0].classList.add('hidden');
+  mobileMenu[0].classList.remove('hidden');
+  mobileNav[0].classList.remove('show');
+});
 
-
-(function() {
-  // Init
-  var container = document.getElementById("img-container"),
-    inner = document.getElementById("inner");
-
-  // Mouse
-  var mouse = {
-    _x: 0,
-    _y: 0,
-    x: 0,
-    y: 0,
-    updatePosition: function(event) {
-      var e = event || window.event;
-      this.x = e.clientX - this._x;
-      this.y = (e.clientY - this._y) * -1;
-    },
-    setOrigin: function(e) {
-      this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
-      this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
-    },
-    show: function() {
-      return "(" + this.x + ", " + this.y + ")";
-    }
-  };
-
-  // Track the mouse position relative to the center of the container.
-  mouse.setOrigin(container);
-
-  //-----------------------------------------
-
-  var counter = 0;
-  var updateRate = 10;
-  var isTimeToUpdate = function() {
-    return counter++ % updateRate === 0;
-  };
-
-  //-----------------------------------------
-
-  var onMouseEnterHandler = function(event) {
-    update(event);
-  };
-
-  var onMouseLeaveHandler = function() {
-    inner.style = "";
-  };
-
-  var onMouseMoveHandler = function(event) {
-    if (isTimeToUpdate()) {
-      update(event);
-    }
-  };
-
-  //-----------------------------------------
-
-  var update = function(event) {
-    mouse.updatePosition(event);
-    updateTransformStyle(
-      (mouse.y / inner.offsetHeight / 2).toFixed(2),
-      (mouse.x / inner.offsetWidth / 2).toFixed(2)
-    );
-  };
-
-  var updateTransformStyle = function(x, y) {
-    var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
-    inner.style.transform = style;
-    inner.style.webkitTransform = style;
-    inner.style.mozTransform = style;
-    inner.style.msTransform = style;
-    inner.style.oTransform = style;
-  };
-
-  //-----------------------------------------
-
-  container.onmouseenter = onMouseEnterHandler;
-  container.onmouseleave = onMouseLeaveHandler;
-  container.onmousemove = onMouseMoveHandler;
-})();
+mobileMenuClose.addEventListener('click', function(){
+  mobileMenu[0].classList.add('hidden');
+  mobileNav[0].classList.add('show');
+  mobileMenu[0].classList.remove('show');
+  mobileNav[0].classList.remove('hidden');
+});
---
+++ slideshow.js
@@ -0,0 +1,31 @@
+//Slideshow functionality
+
+const prev = document.getElementById('button_left');
+const next = document.getElementById('button_right');
+const figure = document.getElementById('slideshow');
+const images = document.getElementsByClassName('slideshow-img');
+
+let index = 0;
+
+prev.addEventListener('click', () => {nextImage('prev')});
+next.addEventListener('click', () => {nextImage('next')});
+
+function nextImage(direction) {
+  
+  if(direction == 'next') {
+    index++;
+    if(index == images.length) {
+      index = 0;
+    }
+  } else {
+    if(index == 0) {
+      index = images.length - 1;
+    } else {
+      index--;
+    }
+  }
+
+  console.log('test ' + index);
+  figure.style.transform = "translateX(-" + index * 100 + "%)";
+  
+};
